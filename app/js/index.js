3ff682d3944ac7a806fd3823d315031d7e9cc7db
Cobbleopolis <cobbleopolis@gmail.com>
2016-11-03 14:41:26 -0400

    Added basic audio to page

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,48 @@
 require("../sass/layout.sass");
 
 console.log(process.env.NODE_ENV);
+
+const bufferSize = 4096;
+
+let audioCtx = new AudioContext();
+
+audioCtx.suspend();
+
+let whiteNoise = audioCtx.createScriptProcessor(bufferSize, 1, 1);
+let brownNoise = audioCtx.createScriptProcessor(bufferSize, 1, 1);
+let masterVolume = audioCtx.createGain();
+
+masterVolume.gain.value = 0.3;
+
+whiteNoise.onaudioprocess = (ev) => {
+    let output = ev.outputBuffer.getChannelData(0);
+    for(let i = 0; i < bufferSize; i++)
+        output[i] = Math.random() * 2 - 1;
+};
+
+{
+    let lastOut = 0.0;
+    brownNoise.onaudioprocess = (ev) => {
+        let input = ev.inputBuffer.getChannelData(0);
+        let output = ev.outputBuffer.getChannelData(0);
+        for (let i = 0; i < bufferSize; i++) {
+            output[i] = (lastOut + (0.02 * input[i])) / 1.02;
+            lastOut = output[i];
+            output[i] *= 3.5;
+        }
+    };
+}
+
+whiteNoise.connect(brownNoise);
+
+brownNoise.connect(masterVolume);
+
+masterVolume.connect(audioCtx.destination);
+
+document.addEventListener('mousedown', () => {
+    audioCtx.resume();
+});
+
+document.addEventListener('mouseup', () => {
+    audioCtx.suspend();
+});
